b8268dc57b87fe23cde3625d515b9e21b5f1dc69
Ivan P <2119240+i5okie@users.noreply.github.com>
2026-05-19 13:27:42 -0700

    Cleanup deprecated code / configuration
    Signed-off-by: Ivan P <2119240+i5okie@users.noreply.github.com>

--- openshift/settings.lcrb.sh
+++
@@ -1,4 +0,0 @@
-# Description: BC Registries profile
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="orgbook-issuer-agent-deploy issuer-wallet-deploy orgbook-issuer-controller-deploy"
-export ignore_templates=""
--- openshift/settings.redis.sh
+++
@@ -1,4 +0,0 @@
-# Description: Shared Redis Cluster Profile - A shared persistent queus for agents.
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="redis-cluster-deploy"
-export ignore_templates=""
